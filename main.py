e7c9beeca611f911d4cf61dd268e64815d135a24
Noam Domovich <>
2022-04-19 01:49:58 +0300

    ddd

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 print("New Work flow Demo")
-print(f"with item id : 2562302585")
-print("Demo 7 with id 2368052731")
-print("Demo 8 with id 2562382205")
-print("Demo 9 with id 2562398203")
-print("Demo 10 with id 2562430692") 
+print("item id 2562456483")
